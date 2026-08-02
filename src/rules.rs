lintje/lintje

commit 38e70873572d068008ab0c761db7cf982b6074ca

Update regex group matcher syntax
This broke after the regex dependency upgrade. Between the two versions
the group matcher syntax was updated and it only showed itself on the
project run.

src/rules.rs
@@ -73,7 +73,7 @@ lazy_static! {
     .unwrap();
     pub static ref CONTAINS_FIX_TICKET_OR_TICKET_REFERENCE: Regex = Regex::new(&format!(
         r"(?xi)
-        (?P<match>(?P<keyword>{FIX_KEY_WORD_REFERENCE}\s+)?{ISSUE_NUMBER_REFERENCE})"
+        (?<match>(?<keyword>{FIX_KEY_WORD_REFERENCE}\s+){ISSUE_NUMBER_REFERENCE}"
     ))
     .unwrap();
     pub static ref CO_AUTHOR_REFERENCE: Regex =

commit f8a87f0815266451f82b39badcced2ad57543502

Fix ticket number keyword regex
Mark the keyword as optional in the ticket number regex.
I accidentally broke this in commit
38e70873572d068008ab0c761db7cf982b6074ca

src/rules.rs
@@ -73,7 +73,7 @@ lazy_static! {
     .unwrap();
     pub static ref CONTAINS_FIX_TICKET_OR_TICKET_REFERENCE: Regex = Regex::new(&format!(
         r"(?xi)
-        (?<match>(?<keyword>{FIX_KEY_WORD_REFERENCE}\s+){ISSUE_NUMBER_REFERENCE})"
+        (?<match>(?<keyword>{FIX_KEY_WORD_REFERENCE}\s+)?{ISSUE_NUMBER_REFERENCE})"
     ))
     .unwrap();
     pub static ref CO_AUTHOR_REFERENCE: Regex =